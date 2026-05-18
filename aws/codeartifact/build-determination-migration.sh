4e3adceb5548174e49e8d22cfe1f4a08214c5040
Jeff Sikala <sikala9@hotmail.com>
2026-05-18 10:09:38 -0400

    finds

--- aws/codeartifact/build-determination-migration.sh
+++ aws/codeartifact/build-determination-migration.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 
 artifactoryRepositoryUrl="https://artifactory.mgmt.cnxs.vpcaas.fcs.gsa.gov/artifactory/api/repositories"
+user="sa_conexus"
+# Set from global Bamboo variables
+api_key="${bamboo_artifactory_admin_api_key_secret}"
 
 longLivedPrefix="ll-"
 BranchName="$(git rev-parse --abbrev-ref HEAD | cut -d'/' -f2)"
 
 RepositoryBaseUrl="https://artifactory.mgmt.cnxs.vpcaas.fcs.gsa.gov/artifactory/"
-
-# Set from global Bamboo variables
-# Bamboo exposes secret variables to scripts with dots replaced by underscores
-access_token="${bamboo_artifactory_admin_api_key_secret}"
-
+ 
 ## Check to see if this is a long lived branch child
 if [[ "${BranchName:0:3}" = "${longLivedPrefix}" ]]; then
     echo "This is a long lived branch"
     # Split the parent and child branch name
     ParentBranchName=$(git rev-parse --abbrev-ref HEAD | cut -d'/' -f2 | cut -d'+' -f1)
     ChildBranchName=$(git rev-parse --abbrev-ref HEAD | cut -d'/' -f2 | cut -d'+' -f2 -s)
-
+    
     # Set the plugin repo to conexus-ll-plugin-repository
     # Set the branch repo to the parent branch
     echo "BranchName=${ParentBranchName}" >> file.properties
@@ -26,62 +25,42 @@ if [[ "${BranchName:0:3}" = "${longLivedPrefix}" ]]; then
     mavenFeatureRepositoryUrl="${RepositoryBaseUrl}${ParentBranchName}/"
     echo "pluginRepositoryUrl=${pluginRepositoryUrl}" >> file.properties
     echo "mavenFeatureRepositoryUrl=${mavenFeatureRepositoryUrl}" >> file.properties
-
+    
     # Check that the long lived parent maven repo exists - if it doesn't create it
-    mavenRepoExists=$(curl -s \
-      -H "Authorization: Bearer ${access_token}" \
-      -X GET "${artifactoryRepositoryUrl}?type=local&packageType=maven" \
-      | grep "${BranchName}" | head -n1)
-
+    mavenRepoExists=$(curl -s -X GET "$artifactoryRepositoryUrl?type=local&packageType=maven" | grep $BranchName | head -n1)
     echo "Checking if repo exists"
     if [[ -z "${mavenRepoExists}" ]]; then
         echo "Repo does not exist. Creating Maven repo: ${BranchName}"
-        curl -s -X PUT \
-          -H "Content-type: application/json" \
-          -H "Authorization: Bearer ${access_token}" \
-          "${artifactoryRepositoryUrl}/${BranchName}" \
-          -d '{ "key": "'${BranchName}'", "rclass" : "local", "packageType": "maven", "repoLayoutRef" : "maven-2-default", "snapshotVersionBehavior": "unique"}'
-
-        echo "Updating permissions on new repo"
+        curl -X PUT \
+           -H "Content-type: application/json" \
+           -H "X-JFrog-Art-Api:$api_key" \
+           "$artifactoryRepositoryUrl/${BranchName}" \
+           -d '{ "key": "'${BranchName}'", "rclass" : "local", "packageType": "maven", "repoLayoutRef" : "maven-2-default", "snapshotVersionBehavior": "unique"}'
+        echo "Updating permsissions on new repo"
         # Getting the anon_read_only permission model, add new repo name, and push changes
-        curl -s -X GET \
-          -H "Authorization: Bearer ${access_token}" \
-          "https://artifactory.mgmt.cnxs.vpcaas.fcs.gsa.gov/artifactory/api/v2/security/permissions/anon_read_only" \
-          | jq ".repo.repositories += [\"${BranchName}\"]" \
-          | curl -s -X PUT \
-              -H "Authorization: Bearer ${access_token}" \
-              -H "Content-Type: application/json" \
-              "https://artifactory.mgmt.cnxs.vpcaas.fcs.gsa.gov/artifactory/api/v2/security/permissions/anon_read_only" \
-              -d@-
-
+        curl -s -X GET -H "X-JFrog-Art-Api:${api_key}" https://artifactory.mgmt.cnxs.vpcaas.fcs.gsa.gov/artifactory/api/v2/security/permissions/anon_read_only \
+            | jq ".repo.repositories += [\"${BranchName}\"]" \
+            | curl -s -X PUT -H "X-JFrog-Art-Api:${api_key}" -H "Content-Type: application/json" https://artifactory.mgmt.cnxs.vpcaas.fcs.gsa.gov/artifactory/api/v2/security/permissions/anon_read_only -d@-
         # Getting the uploadOnly permission model, add new repo name, and push changes
-        curl -s -X GET \
-          -H "Authorization: Bearer ${access_token}" \
-          "https://artifactory.mgmt.cnxs.vpcaas.fcs.gsa.gov/artifactory/api/v2/security/permissions/uploadOnly" \
-          | jq ".repo.repositories += [\"${BranchName}\"]" \
-          | curl -s -X PUT \
-              -H "Authorization: Bearer ${access_token}" \
-              -H "Content-Type: application/json" \
-              "https://artifactory.mgmt.cnxs.vpcaas.fcs.gsa.gov/artifactory/api/v2/security/permissions/uploadOnly" \
-              -d@-
+        curl -s -X GET -H "X-JFrog-Art-Api:${api_key}" https://artifactory.mgmt.cnxs.vpcaas.fcs.gsa.gov/artifactory/api/v2/security/permissions/uploadOnly \
+            | jq ".repo.repositories += [\"${BranchName}\"]" \
+            | curl -s -X PUT -H "X-JFrog-Art-Api:${api_key}" -H "Content-Type: application/json" https://artifactory.mgmt.cnxs.vpcaas.fcs.gsa.gov/artifactory/api/v2/security/permissions/uploadOnly -d@-
     else
         echo "Repo does exist"
     fi
-
 elif [[ ! "${BranchName:0:3}" = "${longLivedPrefix}" ]]; then
     echo "Not a long lived branch"
     # Not a long lived branch
     BranchName=$(git rev-parse --abbrev-ref HEAD | cut -d'/' -f2)
 
-    # Set the plugin repo to conexus-plugin-repository
-    # Set the feature repo to conexus-snapshot-local for SNAPSHOT artifact storage
+    # Set the plugin repo to conexus--plugin-repository
+    # Set the branch repo to the parent branch
     echo "BranchName=${BranchName}" >> file.properties
     pluginRepositoryUrl="${RepositoryBaseUrl}conexus-plugin-repository/"
-    mavenFeatureRepositoryUrl="${RepositoryBaseUrl}conexus-snapshot-local/"
+    mavenFeatureRepositoryUrl="${RepositoryBaseUrl}conexus-plugin-repository/"
     echo "pluginRepositoryUrl=${pluginRepositoryUrl}" >> file.properties
     echo "mavenFeatureRepositoryUrl=${mavenFeatureRepositoryUrl}" >> file.properties
-
 else
-    echo "Cannot determine parent branch"
-    exit 1
+      echo "Cannot determine parent branch"
+      exit 1
 fi
